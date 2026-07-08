ec0f0689afedb74ae210d2aac4e3eaa5a98a016d
CodingMarkus <CodingMarkus.Commit.2022@quantentunnel.de>
2026-07-08 03:40:05 +0200

    Fix unit test to work on darwin
    Fixes #7

--- test-random-input.sh
+++ test-random-input.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 export LC_CTYPE=C
+export LC_ALL=C
+
+output=./build/cminify
 for format in css js xml html json; do
 	for i in {0..1000}; do
-		input=$(tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 1000)
-		./build/native/cminify $format <(echo -n -e "$input") > /dev/null
+		input=$(LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' \
+			</dev/urandom | head -c 1000)
+		"$output" "$format" <(printf '%s' "$input") > /dev/null 2>&1
 		if [ $? -gt 1 ]; then
-			echo -n $input > input-causing-crash.txt
-			echo Crash to reproduce: cminify $format input-causing-crash.txt
+			printf '%s' "$input" > input-causing-crash.txt
+			echo Crash to reproduce: cminify "$format" input-causing-crash.txt
 			exit
 		fi
 	done
